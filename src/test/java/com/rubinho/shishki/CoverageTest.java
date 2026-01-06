e96114d2e2e7a77e92a8e8b939709427bb912a0b
rubinho-m <rubinho-m@yandex.ru>
2026-01-06 16:02:44 +0400

    jacoco

---
+++ src/test/java/com/rubinho/shishki/CoverageTest.java
@@ -0,0 +1,12 @@
+package com.rubinho.shishki;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CoverageTest {
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
